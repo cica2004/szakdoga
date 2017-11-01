5237422e8cbfeb8c29c1e64dd05f093d537fa16c
cica2004 <32438151+cica2004@users.noreply.github.com>
2017-11-01 22:49:05 +0100

    7
    7

--- mozgoatlag.m
+++ mozgoatlag.m
@@ -1,8 +1,13 @@
-function y = mozgoatlag(x,n)
+function y = mozgoatlag(vector, tav)
 % Mogzoatlag
-y = zeros(size(x,1),1);
-for i = (1+n) : (size(x,1)-n),
-    y(i) = mean(x(i-n:i+n));
+% vector : az a vector amit átlagolni szeretnénk
+% tav : az aktuális elemtõl a legtávolabb lévõ még beszámító elem mindkét
+% irányba ha a tav = 3 és éppen az i-dik elemnél járunk
+% akkor i-3 + i-2 .. i .. + i+2 + i+3 elemek átlagát vesszük
+
+y = zeros(size(vector,1),1);
+for i = (1+tav) : (size(vector,1)-tav),
+    y(i) = mean(vector(i-tav : i+tav));
 end
-y = y(1+n : size(x,1)-n,1);
+y = y(1+tav : size(vector,1)-tav, 1);
 
